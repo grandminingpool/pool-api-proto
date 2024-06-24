72a523ff41a2247d455e45f1b290052335b74a36
Viktor Schastnyy <victor92sh@gmail.com>
2024-06-24 12:21:35 +0300

    feat: changed proto files structure

--- gen.go
+++ gen.go
@@ -1,3 +1,5 @@
-//go:generate protoc --proto_path=pool_api --go_out=./pool_api --go_opt=paths=source_relative --go-grpc_out=./pool_api --go-grpc_opt=paths=source_relative pool_api.proto
+//go:generate protoc --proto_path=proto --go_out=./generated/pool --go_opt=paths=source_relative --go-grpc_out=./generated/pool --go-grpc_opt=paths=source_relative pool.proto
+//go:generate protoc --proto_path=proto --go_out=./generated/pool_miners --go_opt=paths=source_relative --go-grpc_out=./generated/pool_miners --go-grpc_opt=paths=source_relative pool_miners.proto
+//go:generate protoc --proto_path=proto --go_out=./generated/pool_payouts --go_opt=paths=source_relative --go-grpc_out=./generated/pool_payouts --go-grpc_opt=paths=source_relative pool_payouts.proto
 
 package proto
